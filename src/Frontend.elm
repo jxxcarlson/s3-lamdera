e11237000c15e315f80d537c7ca24ba3925bea16
James Carlson <jxxcarlson@gmail.com>
2020-12-08 23:31:35 -0500

    Initial commit: bare app

---
+++ src/Frontend.elm
@@ -0,0 +1,81 @@
+module Frontend exposing (..)
+
+import Browser exposing (UrlRequest(..))
+import Browser.Navigation as Nav
+import Element.Background as Background
+import Element.Font as Font
+import Element.Border as Border
+import Element exposing(..)
+import Html exposing(Html)
+import Lamdera
+import Types exposing (..)
+import Url
+
+
+type alias Model =
+    FrontendModel
+
+
+app =
+    Lamdera.frontend
+        { init = init
+        , onUrlRequest = UrlClicked
+        , onUrlChange = UrlChanged
+        , update = update
+        , updateFromBackend = updateFromBackend
+        , subscriptions = \m -> Sub.none
+        , view = view
+        }
+
+
+init : Url.Url -> Nav.Key -> ( Model, Cmd FrontendMsg )
+init url key =
+    ( { key = key
+      , message = "Welcome to Lamdera! You're looking at the auto-generated base implementation. Check out src/Frontend.elm to start coding!"
+      }
+    , Cmd.none
+    )
+
+
+update : FrontendMsg -> Model -> ( Model, Cmd FrontendMsg )
+update msg model =
+    case msg of
+        UrlClicked urlRequest ->
+            case urlRequest of
+                Internal url ->
+                    ( model
+                    , Cmd.batch [ Nav.pushUrl model.key (Url.toString url) ]
+                    )
+
+                External url ->
+                    ( model
+                    , Nav.load url
+                    )
+
+        UrlChanged url ->
+            ( model, Cmd.none )
+
+        NoOpFrontendMsg ->
+            ( model, Cmd.none )
+
+
+updateFromBackend : ToFrontend -> Model -> ( Model, Cmd FrontendMsg )
+updateFromBackend msg model =
+    case msg of
+        NoOpToFrontend ->
+            ( model, Cmd.none )
+
+view : Model -> Browser.Document FrontendMsg
+view model =
+      {  title ="S3 uploader"
+        , body = [Element.layout [] mainView]
+       }
+
+
+mainView : Element msg
+mainView =
+    column [ width (px 500), centerX, centerY, spacing 30 ]
+        [ el [Font.size 56] (text "S3 Uploader")
+        ]
+
+
